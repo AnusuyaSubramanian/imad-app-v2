acc7eb98cba144ad4ef63cc362b80a3ec466129c
AnusuyaSubramanian <anublushblossom@gmail.com>
2017-02-13 12:17:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,9 +84,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:ArticleName', function(req, res){
+app.get('/:articleName', function(req, res){
     var ArticleName = req.params.ArticleName;
-    res.send(createTemplate(Articles[ArticleName]));
+    res.send(createTemplate(Articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
