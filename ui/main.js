ddea1665e37b460b4c76abb49e20be19317a619d
AnusuyaSubramanian <anublushblossom@gmail.com>
2017-02-15 11:41:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,15 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-//Move an Image
-var img = document.getElementById('img');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+button.onclick = function () {
+    
+    //make request to the counter endpoint
+    
+    //capture the response and store it in a variable
+    
+    //render the variable in the correct span.
+    counter  = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 }
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
-};
